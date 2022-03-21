ceb311e6ddf99bf9f4088a2f05df1b3c7b9b6907
EdvinasKun <95015189+EdvinasKun@users.noreply.github.com>
2022-03-21 23:07:57 +0200

    First commit

---
+++ src/components/Footer.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Footer() {
+  return (
+    <div>
+        <footer className="text-muted">
+      <div className="container">
+        <p className="float-right">
+          <a href="https://getbootstrap.com/docs/4.0/examples/album/#">Back to top</a>
+        </p>
+        <p>Album example is © Bootstrap, but please download and customize it for yourself!</p>
+        <p>New to Bootstrap? <a href="https://getbootstrap.com/docs/4.0/">Visit the homepage</a> or read our <a href="https://getbootstrap.com/docs/4.0/getting-started/">getting started guide</a>.</p>
+      </div>
+    </footer>
+    </div>
+  )
+}
+
+export default Footer
